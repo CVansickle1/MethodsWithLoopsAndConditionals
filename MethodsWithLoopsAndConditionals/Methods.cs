aced1fc31e0fb1a63a602f323a494fcc86cf84de
Cody <Cojo341@gmail.com>
2022-08-12 21:55:52 -0400

    Initial Commit

---
+++ MethodsWithLoopsAndConditionals/Methods.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Methods
+    {
+
+        public static int GetInput()
+        {
+            bool numCheck;
+
+            numCheck = int.TryParse(Console.ReadLine(), out int num);
+
+            while (!numCheck)
+            {
+                Console.WriteLine("Please enter a number: ");
+                numCheck = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            return num;
+        }
+
+
+        public static void RangeFinder(int firstNum, int secondNum)
+        {
+            var myLargelist = new List<int>();
+
+            if (firstNum > secondNum)
+            {
+                do
+                {
+                    myLargelist.Add(firstNum);
+                    firstNum--;
+                }
+
+                while (firstNum >= secondNum);
+
+                Console.WriteLine("Here is the range of those two numbers: \n");
+
+                foreach (int x in myLargelist)
+                {
+
+                    Console.WriteLine(x);
+                }
+            }
+
+            else if (secondNum > firstNum)
+            {
+                do
+                {
+                    myLargelist.Add(firstNum);
+                    firstNum++;
+                }
+
+                while (secondNum >= firstNum);
+
+                Console.WriteLine("Here is the range of those two numbers: \n");
+                foreach (int x in myLargelist)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Those two numbers are equal!");
+
+            }
+        }
+
+        public static void TimesThreeRange(int startingNum, int expectedNum)
+        {
+            var myThreelist = new List<int>();
+
+
+            if (startingNum < expectedNum)
+            {
+                Console.WriteLine($"Here is the range of those two numbers by an interval of 3: \n");
+                do
+                {
+                    myThreelist.Add(startingNum);
+                    startingNum += 3;
+                }
+
+                while (startingNum <= expectedNum);
+
+
+                foreach (int x in myThreelist)
+                {
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine(expectedNum);
+            }
+
+            else if (startingNum > expectedNum)
+            {
+                Console.WriteLine($"Here is the range of those two numbers by an interval of 3: \n");
+
+                do
+                {
+                    myThreelist.Add(startingNum);
+                    startingNum -= 3;
+                }
+
+                while (startingNum >= expectedNum);
+
+
+                foreach (int x in myThreelist)
+                {
+                    Console.WriteLine(x);
+                }
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Those two numbers are equal!");
+            }
+
+
+        }
+
+        public static string EqualOrNot(int isThis, int equalTo)
+        {
+            return (isThis == equalTo) ? $"Yup, {isThis} is equal to {equalTo} " : $"Nope, {isThis} is not equal to {equalTo}";
+        }
+
+        public static string Even(int evenORnot)
+        {
+            return (evenORnot % 2 == 0) ? $"{evenORnot} is EVEN" : $"{evenORnot} is ODD";
+        }
+
+        public static string IsThisPositive(int userInput)
+        {
+            if (userInput > 0)
+            {
+                return "This is Positive!";
+            }
+            else if (userInput < 0)
+            {
+                return "This is Negative";
+            }
+            else
+            {
+                return "Thats 0!";
+            }
+
+        }
+        public static string CanYouVote(int age)
+        {
+            return (age >= 18) ? "Yes! You can vote! Dont forget to register in your area! :)" : "Sorry! You cannot vote at the moment, you have to be at least 18 years old :(";
+        }
+        
+       public static string IsItInRange(int rangeCheck)
+        {
+            var tenList = new List<int>();
+            var startNum = -10;
+            
+            do
+            {
+                tenList.Add(startNum);
+                startNum++;
+            }
+            while (startNum <= 10);
+
+            return (tenList.Contains(rangeCheck)) ? "That number is in range of -10 - 10!" : "That number is not in range of -10 - 10";
+
+        }
+
+        public static void MulplicationTable(int initialVal)
+        {
+            var Table = new List<int>();
+            var results = new List<int>();
+            
+
+            for (int i = 1; i <= 12; i++)
+            {
+                Table.Add(i);
+       
+                do
+                {
+                    var finalResults = initialVal * i;
+                    results.Add(finalResults);
+                }
+                while (Table.Count >= 13);
+            }
+
+            foreach (int x in results)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,84 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+
+
+
+
+Console.WriteLine("Please Provide 2 numbers you would like to see the range of:");
+Console.WriteLine("Enter Your First number: ");
+int firstInput = Methods.GetInput();
+
+Console.WriteLine("Enter a Second number: ");
+int secondInput = Methods.GetInput();
+
+Methods.RangeFinder(firstInput, secondInput);
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Please Provide 2 numbers you would like to see the range with an interval of 3: ");
+Console.WriteLine("Enter Your First number: ");
+
+int startingNum = Methods.GetInput();
+
+Console.WriteLine("Enter a Second number: ");
+int expectedNum = Methods.GetInput();
+
+Methods.TimesThreeRange(startingNum, expectedNum);
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Are These two numbers equal?");
+Console.WriteLine("Enter the First number: ");
+
+int areThese = Methods.GetInput();
+
+Console.WriteLine("Enter a Second number: ");
+int equal = Methods.GetInput();
+
+Console.WriteLine(Methods.EqualOrNot(areThese, equal));
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("To find out if the number is even or odd enter it below");
+
+int myInt = Methods.GetInput();
+
+Console.WriteLine(Methods.Even(myInt));
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Is your number Positive or Negative?");
+Console.WriteLine("Enter Below to Test: ");
+
+var positivelyNegative = Methods.GetInput();
+
+Console.WriteLine(Methods.IsThisPositive(positivelyNegative));
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Can you vote?");
+Console.WriteLine("Enter your age to find out!");
+
+var age = Methods.GetInput();
+
+Console.WriteLine(Methods.CanYouVote(age));
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Pick a number -10 - 10");
+
+var inRange = Methods.GetInput();
+
+Console.WriteLine(Methods.IsItInRange(inRange));
+
+Console.WriteLine("\n----------------------------------\n");
+
+Console.WriteLine("Enter a Number you would like to see the multiplication table of (Up to x12): ");
+
+var initialVal = Methods.GetInput();
+
+Console.WriteLine("-------------------------------");
+Methods.MulplicationTable(initialVal);
+
+
+
